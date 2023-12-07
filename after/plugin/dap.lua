ce3464c6049082d93688c010768e4c0796bc64ee
AnoHito <xXAnoHitoXx@users.noreply.github.com>
2023-12-07 00:14:30 -0400

    dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,7 @@
 local dap = require('dap')
+local dap_ui = require("dapui")
+require("nvim-dap-virtual-text").setup()
+
 dap.adapters.godot = {
   type = "server",
   host = '127.0.0.1',
@@ -14,3 +17,10 @@ dap.configurations.gdscript = {
     launch_scene = true,
   }
 }
+
+vim.keymap.set("n", "<leader>td", function() dap_ui.toggle() end)
+vim.keymap.set("n", "<leader>tt", function() dap.continue() end)
+vim.keymap.set('n', "<leader>ts", function() dap.step_over() end)
+vim.keymap.set('n', "<leader>ti", function() dap.step_into() end)
+vim.keymap.set('n', "<leader>to", function() dap.step_out() end)
+vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
--- lua/anohito/packer.lua
+++ lua/anohito/packer.lua
@@ -42,4 +42,6 @@ return require('packer').startup(function(use)
         }
     }
     use 'mfussenegger/nvim-dap'
+    use 'rcarriga/nvim-dap-ui'
+    use 'theHamsta/nvim-dap-virtual-text'
 end)
